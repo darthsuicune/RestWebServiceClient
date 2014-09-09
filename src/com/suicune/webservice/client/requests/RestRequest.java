c7838955bb3eced303ece0f95be9170c9d8b9086
Denís <lapuente@abbyy.com>
2014-09-09 17:59:55 +0200

    Initial Commit

--- src/com/suicune/webservice/client/requests/RestRequest.java
+++ src/com/suicune/webservice/client/requests/RestRequest.java
@@ -4,4 +4,6 @@ package com.suicune.webservice.client.requests;
  * Created by lapuente on 09.09.14.
  */
 public interface RestRequest {
+	public String getResource();
+	public String getFormat();
 }
--- src/com/suicune/webservice/client/server_definition/ServerDefinition.java
+++ src/com/suicune/webservice/client/server_definition/ServerDefinition.java
@@ -8,6 +8,7 @@ public interface ServerDefinition {
 	public static final String PROTOCOL_HTTPS = "https";
 	public static final int DEFAULT_PORT_HTTP = 80;
 	public static final int DEFAULT_PORT_HTTPS = 443;
+
 	public String getUrl();
 	public String getProtocol();
 	public String getPort();
